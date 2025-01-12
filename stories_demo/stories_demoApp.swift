203e9b7acc3edc036e94538db6cc0e0a87bd5c0d
amr fadl <afadl@worldofss.com>
2025-01-12 20:40:55 +0200

    Initial Commit

---
+++ stories_demo/stories_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  stories_demoApp.swift
+//  stories_demo
+//
+//  Created by amr ibrahim on 12/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct stories_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
